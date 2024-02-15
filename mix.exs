e2a26835eb6bc23c95ecdf4870c1234a42491b65
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-02-15 17:57:48 +0530

    - Added credo, sobelow, dialyzer and coveralls

--- mix.exs
+++ mix.exs
@@ -57,7 +57,12 @@ defmodule SnowPortal.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.18", only: :test},
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
