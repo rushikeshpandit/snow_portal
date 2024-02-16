23432a0a0c4c3e660aa4fd09069ed650fc63b8ff
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-02-16 17:55:54 +0530

    - Updated credo config

--- .credo.exs
+++ .credo.exs
@@ -12,9 +12,12 @@
         {Credo.Check.Readability.ModuleDoc, false},
         {Credo.Check.Consistency.TabsOrSpaces},
         {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 100},
-        {Credo.Check.Design.TagTODO, exit_status: 2},
+        {Credo.Check.Design.TagTODO, exit_status: 0},
         {Credo.Check.Refactor.MapInto, false},
         {Credo.Check.Warning.LazyLogging, false}
+      ],
+      disabled: [
+        {Credo.Check.Warning.WrongTestFileExtension}
       ]
     }
   ]
