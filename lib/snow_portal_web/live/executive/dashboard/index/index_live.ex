917c2fd2ec4d28141d44628fcb2cde6422e817f8
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-02-20 13:19:52 +0530

    - Added dummy dashboard page for executive

---
+++ lib/snow_portal_web/live/executive/dashboard/index/index_live.ex
@@ -0,0 +1,17 @@
+defmodule SnowPortalWeb.Executive.Dashboard.IndexLive do
+  use SnowPortalWeb, :live_view
+
+  def handle_params(params, _uri, socket) do
+    live_action = socket.assigns.live_action
+
+    socket =
+      socket
+      |> apply_action(live_action, params)
+
+    {:noreply, socket}
+  end
+
+  defp apply_action(socket, :new, _params) do
+    socket |> assign(:page_title, "Executive Dashboard")
+  end
+end
