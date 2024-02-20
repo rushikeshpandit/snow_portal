50385b479ecdef28d57d4837f61050e490d41519
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-02-20 14:51:30 +0530

    - Added users using seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,28 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     SnowPortal.Repo.insert!(%SnowPortal.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias SnowPortal.Accounts
+
+Accounts.register_user(%{
+  email: "user@mobilixir.com",
+  password: "User@Mobilixir11",
+  first_name: "User",
+  last_name: "Mobilixir",
+  user_name: "user",
+  role: "USER"
+})
+
+Accounts.register_user(%{
+  email: "admin@mobilixir.com",
+  password: "Admin@Mobilixir11",
+  first_name: "Admin",
+  last_name: "Mobilixir",
+  user_name: "admin",
+  role: "ADMIN"
+})
+
+Accounts.register_user(%{
+  email: "executive@mobilixir.com",
+  password: "Executive@Mobilixir11",
+  first_name: "Executive",
+  last_name: "Mobilixir",
+  user_name: "executive",
+  role: "EXECUTIVE"
+})
