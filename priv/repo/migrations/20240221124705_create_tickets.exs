3c29b45ab645e663d79550152a4664a4756f8fc6
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-04-02 20:03:46 +0530

    - Added created by user id and assigned to user id for ticket migration

--- priv/repo/migrations/20240221124705_create_tickets.exs
+++ priv/repo/migrations/20240221124705_create_tickets.exs
@@ -20,10 +20,16 @@ defmodule SnowPortal.Repo.Migrations.CreateTickets do
       add :description, :string
       add :priority, :priority_types, default: "LOW", null: false
 
-      add :user_id,
-          references(:users, on_delete: :delete_all, on_update: :update_all, type: :binary_id)
+      add :created_by_user_id,
+          references(:users, on_delete: :nothing, type: :binary_id)
+
+      add :assigned_to_user_id,
+          references(:users, on_delete: :nothing, type: :binary_id)
 
       timestamps(type: :utc_datetime)
     end
+
+    create index(:tickets, [:created_by_user_id])
+    create index(:tickets, [:assigned_to_user_id])
   end
 end
