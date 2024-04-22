d0d1f6fc8336f0afb83cedb43154401c80401bcd
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-04-22 15:37:48 +0530

    - Added ticket listing page to executive

---
+++ lib/snow_portal_web/live/executive/list_live/tickets/index.ex
@@ -0,0 +1,26 @@
+defmodule SnowPortalWeb.Executive.ListLive.Tickets.Index do
+  use SnowPortalWeb, :live_view
+
+  alias SnowPortal.Tickets
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, stream(socket, :tickets, Tickets.list_tickets_by_user(socket.assigns.current_user.id))}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :index, _params) do
+    socket
+    |> assign(:page_title, "Listing Tickets")
+    |> assign(:ticket, nil)
+  end
+
+  @impl true
+  def handle_info({SnowPortalWeb.Customer.TicketLive.FormComponent, {:saved, ticket}}, socket) do
+    {:noreply, stream_insert(socket, :tickets, ticket)}
+  end
+end
