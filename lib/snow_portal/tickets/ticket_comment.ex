4431bcb9d7d0d5944412e26370e9473991dae394
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-05-03 19:12:40 +0530

    - Updated schema name and context name

--- lib/snow_portal/tickets/ticket_comment.ex
+++ lib/snow_portal/tickets/ticket_comment.ex
@@ -1,4 +1,4 @@
-defmodule SnowPortal.Tickets.TicketComments do
+defmodule SnowPortal.Tickets.TicketComment do
   alias SnowPortal.Tickets.Ticket
   alias SnowPortal.Accounts.User
   use Ecto.Schema
@@ -6,7 +6,7 @@ defmodule SnowPortal.Tickets.TicketComments do
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
-  schema "ticket_comments" do
+  schema "ticket_comment" do
     field :comment, :string
 
     belongs_to :ticket, Ticket
@@ -15,9 +15,10 @@ defmodule SnowPortal.Tickets.TicketComments do
     timestamps(type: :utc_datetime)
   end
 
-  def changeset(ticket_comments, attrs) do
-    ticket_comments
+  def changeset(ticket_comment, attrs) do
+    ticket_comment
     |> cast(attrs, [:ticket_id, :user_id, :comment])
     |> validate_required([:comment])
+    |> validate_length(:comment, min: 4)
   end
 end
